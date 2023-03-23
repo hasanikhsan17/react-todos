47fc51bf0a68fb6b21149b1c2ed484afba792107
hb-17 <softdevhasan@gmail.com>
2023-03-23 14:47:44 +0700

    17.Create a Second React Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import TodoRowItem from "./component/TodoRowItem";
+import TodoTable from "./component/TodoTable";
 
 function App() {
   const todos = [
@@ -12,37 +13,7 @@ function App() {
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Description</th>
-                <th scope="col">Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <TodoRowItem
-                rowNumber={todos[0].rowNumber}
-                rowDescription={todos[0].rowDescription}
-                rowAssigned={todos[0].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={todos[1].rowNumber}
-                rowDescription={todos[1].rowDescription}
-                rowAssigned={todos[1].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={todos[2].rowNumber}
-                rowDescription={todos[2].rowDescription}
-                rowAssigned={todos[2].rowAssigned}
-              />
-              <tr>
-                <th scope="col">2</th>
-                <td>Get haircut</td>
-                <td>Eric</td>
-              </tr>
-            </tbody>
-          </table>
+          <TodoTable todos={todos} />
         </div>
       </div>
     </div>
